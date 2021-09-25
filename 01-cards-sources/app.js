f2a9c3a2444a40c7e9b13a180b30b68b05e31301
Localbody <localbody@gmail.com>
2021-09-25 15:36:46 +0300

    first commit

---
+++ 01-cards-sources/app.js
@@ -0,0 +1,16 @@
+const slides = document.querySelectorAll('.slide');
+
+for (const slide of slides ) {
+
+    slide.addEventListener('click', () => {
+
+        clearActive();
+        slide.classList.add('active');
+    })
+
+}
+function clearActive() {
+    slides.forEach( (slide) => {
+        slide.classList.remove('active');
+    } )
+}
